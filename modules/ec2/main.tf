c5bdbbec57b00ffe50d988e8639c393081965263
Alejandro Daza Iglesias <adaza@vectorim.net>
2019-12-16 13:29:38 +0000

    Added launch template base resource

---
+++ modules/ec2/main.tf
@@ -0,0 +1,36 @@
+resource "aws_launch_template" "dev-wp-base-launchtemplate" { // CONFIGURACION DE LANZAMIENTO DE PLANTILLA.
+  name_prefix   = "dev-wp-base-launchtemplate"
+  image_id      =   "ami-031de832435c04744"
+  instance_type           = t2.micro"
+  key_name                = "dev-tf-wp-launch-template"
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = {
+        Name = "wp-base"
+        Environment = "Development"
+        SLA = "8x5"
+        Project = "Wordpress Base"
+        IaC = "Terraform"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+    tags          = {
+        Name = "wp-base"
+        Environment = "Development"
+        SLA = "8x5"
+        Project = "Wordpress Base"
+        IaC = "Terraform"
+    }
+  }
+
+  tags = {
+        Name = "wp-base"
+        Environment = "Development"
+        SLA = "8x5"
+        Project = "Wordpress Base"
+        IaC = "Terraform"
+  }
+}
