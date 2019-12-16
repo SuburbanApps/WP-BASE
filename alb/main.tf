055f675540a269e7288ee6ce71a77ed5442b3e6c
Alejandro Daza Iglesias <adaza@vectorim.net>
2019-12-16 12:25:14 +0000

    Updated main.tf

--- alb/main.tf
+++ alb/main.tf
@@ -1,7 +1,38 @@
 #Configuracion ALB
 
-resource "aws_lb" "tf-wp-balanceador" {
-  name               = "tfwpalb"
+resource "aws_security_group" "wp-alb-sg" { // myportal ALB group
+  name        = "dev-wp-alb-sg" //hacerlo mas adelante con variables.
+  description = "SG for TPZCom Public Alb"
+  vpc_id      = "${var.mivpc}" //"${local.vpc_id}"
+
+  ingress {
+    description = "From All"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "From All"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  
+}
+
+resource "aws_lb" "dev10-tf-wp-alb" {
+  name               = "Balanceador"
   internal           = false
   load_balancer_type = "application"
   #security_groups    = ["${aws_security_group.tf_wp_AlbSecurityGroup.id}"]
