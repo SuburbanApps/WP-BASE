66ba6c31469a0011fe75979eba09d830e07f79e7
Alejandro Daza Iglesias <adaza@vectorim.net>
2019-12-16 12:30:28 +0000

    Updated variables.tf

--- alb/variables.tf
+++ alb/variables.tf
@@ -1,1 +1,11 @@
 #variables del Balanceador.
+
+variable "subredes" {
+  type    = "list"
+  default = ["subnet-650ae83e", "subnet-ea19bc8d" , "subnet-c9cd6f80"]
+}
+
+variable "vpcidentificador" {
+  type    = "string"
+  default = "vpc-dc76d7bb"
+}
