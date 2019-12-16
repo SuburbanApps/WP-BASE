51c109f599e593d8425eeafed83740272d22385a
Alejandro Daza Iglesias <adaza@vectorim.net>
2019-12-16 12:15:28 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
 #variables globales, comunes a todos los módulos.
+
+variable "subredes" {
+  type    = "list"
+  default = ["subnet-650ae83e", "subnet-ea19bc8d" , "subnet-c9cd6f80"]
+}
+
+variable "vpcidentificador" {
+  type    = "string"
+  default = "vpc-dc76d7bb"
+}
