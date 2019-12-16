146cee94e498331de40db8e4718e3f35c3f61018
Alejandro Daza Iglesias <adaza@vectorim.net>
2019-12-16 13:43:53 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,8 @@ variable "vpc_id" {
     type = "string"
     default = "vpc-dc76d7bb"
 }
+
+variable "subredes" {
+  type    = "list"
+  default = ["subnet-650ae83e", "subnet-ea19bc8d" , "subnet-c9cd6f80"]
+}
