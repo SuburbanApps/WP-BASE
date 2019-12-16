2712886a5c68b476beee8bc03003e1fbeba1ff0a
Alejandro Daza Iglesias <adaza@vectorim.net>
2019-12-16 12:53:29 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,13 @@
 provider "aws" {
     region ="eu-west-1"
 }
-  
+
 terraform {
   backend "s3" {
-    bucket = "arn:aws:s3:::vim-terraform-backend"
-    dynamodb_table = "arn:aws:dynamodb:eu-west-1:190761377083:table/vim-terraform-backend"
-    key = "myportal"
-    role_arn = "arn:aws:iam::143491526082:role/TerraformAccess"
-    profile = "terraform"
+    bucket = "vim-terraform-backend"
+    dynamodb_table = "vim-terraform-backend"
+    key = "wp-base.tfstate"
     region = "eu-west-1"
     encrypt = true
-    kms_key_id = "arn:aws:kms:eu-west-1:143491526082:key/7f72131a-43a1-49d8-aab0-9eab6ee66178"
   }
 }
