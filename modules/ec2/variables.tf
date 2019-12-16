3cff14fe7cdafcd98d6e6c435a95742da226d689
Alexis Martín Montero <amartinmo@vectorim.net>
2019-12-16 15:27:58 +0000

    Updated variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,3 +2,8 @@ variable "vpc_id" {
     description = "VPC ID where the compute resources will be created."
     type = "string"
 }
+
+variable "security_group_ids" {
+    description = "List of security group IDs that will apply to this compute resources"
+    type = list(string)
+}
