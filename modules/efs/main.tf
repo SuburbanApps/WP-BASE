ec25d0ae05eda1b02944bab0d5dfcd82f360ba28
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-17 16:22:28 +0100

    test

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -3,16 +3,6 @@ resource "aws_security_group" "dv10-sg-wp-base-efs" {
   description = "SG for WP BASE EFS"
   vpc_id      = "${varvpc_id}"
    
-
-  ingress {
-    description     = "Acceso a instancias"
-    from_port       = 2049
-    to_port         = 2049
-    protocol        = "tcp"
-    security_groups = ["${aws_security_group.dv10-sg-wp-base-instances.id}"]
-
-  }
-
 }
 resource "aws_efs_file_system" "dv10-efs-wp-base" {
   creation_token = "Efs Wordpress Test"
