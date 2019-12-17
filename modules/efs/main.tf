c9973c7700f57ad4f3da4cdb1148e7b2348874ba
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-17 12:01:15 +0100

    added tags

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -1,7 +1,14 @@
 resource "aws_efs_file_system" "dv10-efs-wp-base" {
   creation_token = "Efs Wordpress Test"
 
-  tags = {
-    Name = "EFS-test"
-  }
+   tag_specifications {
+    resource_type = "EFS"
+    tags          = {
+        Name = "wp-base"
+        Environment = "Development"
+        SLA = "8x5"
+        Project = "Wordpress Base"
+        IaC = "Terraform"
+    }
+   }
 }
