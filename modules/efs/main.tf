9b09cf54faa8abfa2ed06b76c8f6d4987d8168b7
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-17 16:16:50 +0100

    create tags

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -2,6 +2,14 @@ resource "aws_security_group" "dv10-sg-wp-base-efs" {
   name        = "sg-wp-base-efs" // hacer con variables
   description = "SG for WP BASE EFS"
   vpc_id      = "${varvpc_id}"
+   tags      = {
+        Name = "wp-base"
+        Environment = "Development"
+        SLA = "8x5"
+        Project = "Wordpress Base"
+        IaC = "Terraform"
+    }
+
   }
 
   ingress {
@@ -13,14 +21,7 @@ resource "aws_security_group" "dv10-sg-wp-base-efs" {
 
   }
 
-  tags      = {
-        Name = "wp-base"
-        Environment = "Development"
-        SLA = "8x5"
-        Project = "Wordpress Base"
-        IaC = "Terraform"
-    }
-
+ 
 resource "aws_efs_file_system" "dv10-efs-wp-base" {
   creation_token = "Efs Wordpress Test"
   
