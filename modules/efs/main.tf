468f95d696e3a5b1361350468803652dfeff8e8c
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-17 16:39:12 +0100

    test

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -1,7 +1,30 @@
 resource "aws_security_group" "dv10-sg-wp-base-efs" { 
-  name        = "sg-wp-base-efs" // hacer con variables
-  description = "SG for WP BASE EFS"
+  name        = "dv10-sg-wp-base-alb" //usar prefix
+  description = "Security Group for EFS"
   vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    description     = "HTTP from internet"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-wp-base-efs"
+    Environment = "Development"
+    Project = "Wordpress Base"
+    IaC = "Terraform"
+  }
+}
    
 }
 resource "aws_efs_file_system" "dv10-efs-wp-base" {
