ecca73133b7ca78d3d24acdf7a08f405db72cf1e
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-18 10:36:43 +0100

    test

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -6,8 +6,8 @@ resource "aws_db_instance" "dv10-db-wp-base" {
   name                    = "WP Base de Datos" #hacer variable
   identifier              = "db-wp-test" ##hacer variable
   #db_subnet_group_name    = "${aws_db_subnet_group.MyportalRDSSubnetGroup.id}"
-  username                = "${data.aws_ssm_parameter.pwd_db.value}"
-  password                = "${data.aws_ssm_parameter.user_db.value}"
+  username                = "${data.aws_ssm_parameter.user_db.value}"
+  password                = "${data.aws_ssm_parameter.pwd_db.value}"
   #vpc_security_group_ids  = ["${aws_security_group.MyportalRDSSecurityGroup.id}"]
   skip_final_snapshot     = true
   multi_az                = "${var.az_select}"
