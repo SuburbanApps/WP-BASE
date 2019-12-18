dde399c1e06f4f2f78b9a2a03e62ea37a0d28058
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-18 09:58:18 +0100

    test error

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -6,3 +6,8 @@ type = "boolean"
 default = false # true in live.
 
 }
+
+variable "private_subnets" {
+    description = "VPC Private Subnets where the compute resources will be created."
+    type = list(string)
+}
