a4b0efd8f85186a32d5c3a25be22db2a8cb0717b
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-18 14:04:02 +0100

    test

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -26,12 +26,25 @@ resource "aws_security_group" "dv10-sg-wp-base-rds" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
-    description     = "From WP Instances"
-    from_port       = 3306
-    to_port         = 3306
+    description     = "HTTP from internet"
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
-    security_groups = ["${aws_security_group.dv10-sg-wp-base-instances.id}"]
-
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dv10-sg-wp-base-alb"
+    Environment = "Development"
+    Project = "Wordpress Base"
+    IaC = "Terraform"
+  }
 }
+
