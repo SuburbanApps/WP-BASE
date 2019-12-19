29ee32aa3ce5101f885234c859e7692b6f16ffaf
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2019-12-19 09:33:26 +0100

    test

--- variables.tf
+++ variables.tf
@@ -1,14 +1,27 @@
 # Global variables
 
 locals {
+
+  aws_region = "eu-west-1"
   vpc_id = "vpc-dc76d7bb"
   private_subnet_ids = ["subnet-650ae83e", "subnet-ea19bc8d" , "subnet-c9cd6f80"]
   public_subnet_ids = ["subnet-c009eb9b","subnet-971cb9f0","subnet-48d67401"]
   key_pair = "dv10-wp-base"
   
-  #user_db = "root"
+  
+enviroment {
 
-  #pwd_db = "123456789"
+  environment_name = {
+      dev     = "Development"
+      staging = "Staging"
+      live    = "Live"
+    }
 
-    
+   environment_prefix = {
+      dev     = "dv10"
+      staging = "st10"
+      live    = "lv10"
+    }
+}
+
 }
