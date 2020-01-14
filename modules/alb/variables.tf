f92a68c04e04bde064b4a9ca3df488ead6ce5de2
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-14 12:33:31 +0100

    env test error

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -7,9 +7,3 @@ variable "public_subnets" {
     description = "VPC Public Subnets where the ALB resources will be created."
     type = list(string)
 }
- locals {
-
-  environment_prefix          = "${lookup(local.env.environment_prefix, terraform.workspace)}"
-  environment_name            = "${lookup(local.env.environment_name, terraform.workspace)}"
- 
-}
