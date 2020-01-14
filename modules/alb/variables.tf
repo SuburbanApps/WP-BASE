df0f88232148b86ae76f0e0d53cad6e695ac419d
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-14 13:05:06 +0100

    env error in apply

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -7,3 +7,14 @@ variable "public_subnets" {
     description = "VPC Public Subnets where the ALB resources will be created."
     type = list(string)
 }
+
+variable "environment_prefix" {
+    description = "enviroments"
+    type = "map"
+    default {
+      dev     = "popotitos"
+      staging = "st10"
+      live    = "lv10"
+    }
+
+    }
