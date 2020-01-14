5157e91fc034887119ffa7a5e6496fd33a131b19
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-14 17:01:40 +0100

    env vars test

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,24 +1,3 @@
- locals {
-
-  environment_prefix          = "${lookup(local.env.environment_prefix, terraform.workspace)}"
-  environment_name            = "${lookup(local.env.environment_name, terraform.workspace)}"
-
- 
-  # env = {
-    #environment_prefix = {
-      #dev     = "dv10"
-      #staging = "st10"
-      #live    = "lv10"
-    #}
-    #environment_name = {
-      #dev     = "Development"
-      #staging = "Staging"
-      #live    = "Live"
-    #}
-   #}
-  
- }
-
 
 resource "aws_security_group" "dv10-sg-wp-base-alb" {
   name        =  "${local.environment_prefix}-sg-wp-base-alb" //error 1
