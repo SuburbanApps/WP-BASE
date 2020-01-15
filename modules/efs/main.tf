30fa0975c5fb6b923dc8f06174a28b3484ba34c2
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-15 12:25:05 +0100

    test

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -60,7 +60,7 @@ resource "aws_efs_file_system" "efs-wp-base" {
     }
 }
 
-resource "aws_efs_mount_target" "dv10-mt-wp-base-efs" {
+resource "aws_efs_mount_target" "mt-wp-base-efs" {
     count = 3
     file_system_id  = "${aws_efs_file_system.efs-wp-base.id}"
     subnet_id       = "${var.private_subnets[count.index]}" 
