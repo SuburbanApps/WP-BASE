4aa423c6bf8ecc5249c4a2725daf60e6c174c2c2
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-16 10:59:16 +0100

    test error

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -7,33 +7,7 @@ variable "public_subnets" {
     description = "VPC Public Subnets where the ALB resources will be created."
     type = list(string)
 }
-locals {
-  environment_prefix          = "${lookup(local.env.environment_prefix, terraform.workspace)}"
-  environment_name            = "${lookup(local.env.environment_name, terraform.workspace)}"
-      env = {
-      environment_prefix = {
-        dev     = "dv10"
-        staging = "st10"
-        live    = "lv10"
-          }
-    
-      environment_name = {
-        dev     = "Development"
-        staging = "Staging"
-        live    = "Live"
-        }
-
-  }
-  
-  
-  not_in_production = "${local.not_in_production_mapping[terraform.workspace]}" 
-  not_in_production_mapping = {
-    dev         = true
-    staging     = true
-    live        = false
-  }
-  
- }
+l
 
 
 
