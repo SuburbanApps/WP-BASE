61c378bb8c98ec30c90676c15150ea62043504f3
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 10:20:21 +0100

    usedata.sh tst

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,8 @@
-#!bin/bash
+#!bin/bash -xe
 sudo yum install -y amazon-efs-utils        
         
         #EFS mount
-        'Fn::Base64': !Sub >
-          #!/bin/bash -xe
-          
-          mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${ElasticFileSystem}.efs.${AWS::Region}.amazonaws.com:/ /opt/apps/htdocs/sites/files
+         mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${ElasticFileSystem}.efs.${AWS::Region}.amazonaws.com:/ /opt/apps/htdocs/sites/files
 
           # Setup fstab to auto mount EFS
 
