56d4b97720d2e8b6c6c1fa4541dffcdbcdd9b39e
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 18:25:54 +0100

    userdata.sh test

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 #1ºinstalacion wordpress
-mkdir ~/var/www/efs
-cd ~/var/www/efs
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar zxf latest.tar.gz
-cd wordpress
-sudo cp -rpf * ../
-cd ../
-sudo rm -rf wordpress/ latest.tar.gz
+mkdir /var/www/efs
+
 
