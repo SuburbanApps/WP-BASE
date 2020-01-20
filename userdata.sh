1c2a44e50ea86f48686133e0782866155a43afa2
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 10:47:08 +0100

    userdata tst

--- userdata.sh
+++ userdata.sh
@@ -2,8 +2,14 @@
 
 sudo yum install -y amazon-efs-utils        
         
-        #EFS mount efs-wp-base
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${efs-wp-base}.efs.${AWS}.amazonaws.com:/ /opt/apps/htdocs/sites/files
-          # Setup fstab to auto mount EFS
+       #Creamos directorio de montaje dentro de la instancia EC2
+       mkdir ~/efs-mount-point 
+       sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,
+       noresvport ${efs-wp-base}.efs.${AWS::eu-west-1}.amazonaws.com:/ ~/efs-mount-point
+       cd ~/efs-mount-point  
+       ls -al
+       sudo chmod go+rw
+       ls -l
 
-          echo "${efs-wp-base}.efs.${AWS::eu-west-1}.amazonaws.com:/ /opt/apps/htdocs/sites/files nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,nofail 0 0" | sudo tee -a /etc/fstab
+
+       
