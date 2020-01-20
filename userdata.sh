7a5741c83c5ae8ac0fb66294e409bd8b1b224957
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 16:55:44 +0100

    userdata.sh tst

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-mkdir /var/www/html/efs
+cd /sites/{{SITE_DOMAIN}}.com/public
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar zxf latest.tar.gz
+cd wordpress
+sudo cp -rpf * ../
+cd ../
+sudo rm -rf wordpress/ latest.tar.gz
 
