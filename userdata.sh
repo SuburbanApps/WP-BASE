112732d74b109b4c2954db4cc42474311b224999
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 18:37:29 +0100

    update usedata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 #1ºinstalacion wordpress
-mkdir /var/www/efs
+#cloud-config
+package_upgrade: true
+packages:
+nfs-utils
+httpd
+php
+runcmd:
+echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-657731ae.efs.eu-west-1.amazonaws.com.efs.us-west-2.amazonaws.com:/    /var/www/html/efs   nfs4    defaults" >> /etc/fstab
+mkdir /var/www/html/efs
+mount -a
+service httpd start
+chkconfig httpd on
 
-
+#!/bin/bash
+#1ºinstalacion wordpress
+mkdir ~/var/www/efs
+cd ~/var/www/efs
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar zxf latest.tar.gz
+cd wordpress
+sudo cp -rpf * ../
+cd ../
+sudo rm -rf wordpress/ latest.tar.gz
