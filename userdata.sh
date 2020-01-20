08c3e654cc125f4fdffee2f76907a081a1e71ab6
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 15:10:44 +0100

    tst userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,12 @@
-#!bin/bash
-sudo yum install -y amazon-efs-utils        
-        
-       #montaje EFS dentro de la instancia EC2
-       mkdir ~/efs-mount-point 
-      sudo mount -t nfs -o
- nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ip-172-31-53-187.eu-west-1.compute.internal:/ ~/efs-mount-point
- cd ~/efs-mount-point
- sudo chmod go+rw . 
-
-
-       
+#cloud-config
+package_upgrade: true
+packages:
+- nfs-utils
+- httpd
+- php
+runcmd:
+- echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-657731ae.efs.eu-west-1.amazonaws.com.efs.us-west-2.amazonaws.com:/    /var/www/html/efs   nfs4    defaults" >> /etc/fstab
+- mkdir /var/www/html/efs
+- mount -a
+- service httpd start
+- chkconfig httpd on
