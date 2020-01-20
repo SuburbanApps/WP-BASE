046d89632c08269c6eb4420a746782bb360bb29d
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 11:11:23 +0100

    userdata.sh tst

--- userdata.sh
+++ userdata.sh
@@ -1,13 +1,9 @@
-
+#!bin/bash
 sudo yum install -y amazon-efs-utils        
         
        #montaje EFS dentro de la instancia EC2
        mkdir ~/efs-mount-point 
-       #sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs-wp-base}.efs.${AWS::eu-west-1}.amazonaws.com:/ ~/efs-mount-point
-       #cd ~/efs-mount-point  
-       #ls -al
-       #sudo chmod go+rw
-       #ls -l
+      
 
 
        
