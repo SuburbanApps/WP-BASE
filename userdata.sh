abcbd78698e76f61c0a3c1d006832eaa6aae79c5
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-20 15:46:17 +0100

    update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,12 @@
 #cloud-config
 package_upgrade: true
 packages:
-- nfs-utils
-- httpd
-- php
+nfs-utils
+httpd
+php
 runcmd:
-- echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-657731ae.efs.eu-west-1.amazonaws.com.efs.us-west-2.amazonaws.com:/    /var/www/html/efs   nfs4    defaults" >> /etc/fstab
-- mkdir /var/www/html/efs
-- mount -a
-- service httpd start
-- chkconfig httpd on
+echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-657731ae.efs.eu-west-1.amazonaws.com.efs.us-west-2.amazonaws.com:/    /var/www/html/efs   nfs4    defaults" >> /etc/fstab
+mkdir /var/www/html/efs
+mount -a
+service httpd start
+chkconfig httpd on
