dd8c7a3da35d1c507e59690bdd4917a46f022625
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 14:12:08 +0100

    update  userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#1Âºinstalacion wordpress
+mkdir ~/var/www/efs
+cd ~/var/www/efs
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar zxf latest.tar.gz
+cd wordpress
+sudo cp -rpf * ../
+cd ../
+sudo rm -rf wordpress/ latest.tar.gz
