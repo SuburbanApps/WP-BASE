2e1e3881c5b72454fb46d24f32d9dce209803645
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 09:56:57 +0100

    update inbound rules

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -51,6 +51,7 @@ resource "aws_security_group" "sg-wp-base-instances" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
     #security_groups = "${var.incoming_sg_ids}"
   }
 
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -85,6 +85,7 @@ resource "aws_security_group" "sg-wp-base-rds" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
     #security_groups = "${var.incoming_sg_ids}"
   }
 
