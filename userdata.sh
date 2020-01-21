839da26a009c873e3dcfa6005a3ede5891c3c8f0
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 17:38:00 +0100

    update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-UserData:
-  Fn::Base64:
-    !Sub |
-      yum update -y
-      yum install -y aws-cfn-bootstrap awscli nfs-utils cloud-init
-      /opt/aws/bin/cfn-init -v --stack ${AWS::StackName} --resource MasterInstances --region ${AWS::Region} -c ascending
-      /opt/aws/bin/cfn-signal -e $? --stack ${AWS::StackName} --resource MasterAutoScalingGroup --region ${AWS::Region}
-      mkdir -p ${EfsMountPoint1}
-      mkdir -p ${EfsMountPoint2}
-      chown ${EfsMountOwner1}:${EfsMountOwner1} ${EfsMountPoint1}
-      chown ${EfsMountOwner2}:${EfsMountOwner2} ${EfsMountPoint2}
+
+mkdir ~/.aws
+touch ~/.aws/config
     
