3b92ffbbcc0d2de259d9df00b39f7c5518d19e07
Alejandro Daza Iglesias <adaza@vectorim.net>
2020-01-21 11:37:04 +0000

    Added variables.tf

---
+++ modules/alb/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {
+    description = "VPC ID where the ALB resources will be created."
+    type = "string"
+}
+
+variable "public_subnets" {
+    description = "VPC Public Subnets where the ALB resources will be created."
+    type = list(string)
+}
+
+
+
+
+
