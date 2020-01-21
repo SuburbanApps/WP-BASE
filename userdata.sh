f8fc419bf54b84973233defaf2f769d965890a67
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 18:13:44 +0100

    update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,11 @@
 
 mkdir ~/.aws
 touch ~/.aws/config
+cd ~/var/www/efs
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar zxf latest.tar.gz
+cd wordpress
+sudo cp -rpf * ../
+cd ../
+sudo rm -rf wordpress/ latest.tar.gz
     
