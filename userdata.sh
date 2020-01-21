8ea2184ba1d9116559bbc9f2ae66d877632ae5b9
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 14:24:47 +0100

    tst mount in userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#1Âºinstalacion wordpress
 mkdir ~/var/www/efs
-cd ~/var/www/efs
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar zxf latest.tar.gz
-cd wordpress
-sudo cp -rpf * ../
-cd ../
-sudo rm -rf wordpress/ latest.tar.gz
+#cloud-config
+package_upgrade: true
+packages:
+nfs-utils
+httpd
+php
+runcmd:
+echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-657731ae.efs.eu-west-1.amazonaws.com:/    /var/www/html/efs   nfs4    defaults" >> /etc/fstab
+mkdir /var/www/html/efs
+mount -a
+service httpd start
+chkconfig httpd on
