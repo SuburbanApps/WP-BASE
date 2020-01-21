e24d85159e23e831f9a99a2c7d05ea09975185fb
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 17:23:31 +0100

    update userdat.sh

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
-#cloud-config
-package_upgrade: true
-packages:
-nfs-utils
-httpd
-php
-runcmd:
-echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-657731ae.efs.eu-west-1.amazonaws.com:/    /var/www/html/efs   nfs4    defaults" >> /etc/fstab
-mount -a
-service httpd start
-chkconfig httpd on
+UserData:
+  Fn::Base64:
+    !Sub |
+      #!/bin/bash -xe
+      yum update -y
+      yum install -y aws-cfn-bootstrap awscli nfs-utils cloud-init
+      /opt/aws/bin/cfn-init -v --stack ${AWS::StackName} --resource MasterInstances --region ${AWS::Region} -c ascending
+      /opt/aws/bin/cfn-signal -e $? --stack ${AWS::StackName} --resource MasterAutoScalingGroup --region ${AWS::Region}
+      mkdir -p ${EfsMountPoint1}
+      mkdir -p ${EfsMountPoint2}
+      chown ${EfsMountOwner1}:${EfsMountOwner1} ${EfsMountPoint1}
+      chown ${EfsMountOwner2}:${EfsMountOwner2} ${EfsMountPoint2}
+      echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${EfsFileSystemId1}.efs.aws-region.amazonaws.com:/ ${EfsMountPoint1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
+      echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${EfsFileSystemId2}.efs.aws-region.amazonaws.com:/ ${EfsMountPoint2} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
+      mount -a -t nfs4
