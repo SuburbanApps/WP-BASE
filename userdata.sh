fdfb9d4caca03e9c6c9256ae28e81b5c13710b76
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 17:27:14 +0100

    update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,6 @@
 UserData:
   Fn::Base64:
     !Sub |
-      #!/bin/bash -xe
       yum update -y
       yum install -y aws-cfn-bootstrap awscli nfs-utils cloud-init
       /opt/aws/bin/cfn-init -v --stack ${AWS::StackName} --resource MasterInstances --region ${AWS::Region} -c ascending
@@ -10,6 +9,4 @@ UserData:
       mkdir -p ${EfsMountPoint2}
       chown ${EfsMountOwner1}:${EfsMountOwner1} ${EfsMountPoint1}
       chown ${EfsMountOwner2}:${EfsMountOwner2} ${EfsMountPoint2}
-      echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${EfsFileSystemId1}.efs.aws-region.amazonaws.com:/ ${EfsMountPoint1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
-      echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${EfsFileSystemId2}.efs.aws-region.amazonaws.com:/ ${EfsMountPoint2} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
-      mount -a -t nfs4
+    
