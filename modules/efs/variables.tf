ece42803df736a143d43a14c533ae94cc58151f6
Alejandro Daza Iglesias <adaza@vectorim.net>
2020-01-21 11:42:25 +0000

    Added variables.tf

---
+++ modules/efs/variables.tf
@@ -0,0 +1,13 @@
+
+variable "vpc_id" {
+    description = "VPC ID where the compute resources will be created."
+    type = "string"
+}
+variable "incoming_sg_ids" {
+    description = "List of security group IDs that will send traffic to this compute resources."
+    type = list(string)
+}
+variable "private_subnets" {
+    description = "VPC Private Subnets where the compute resources will be created."
+    type = list(string)
+}
