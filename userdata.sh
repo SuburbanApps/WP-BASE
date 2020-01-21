45cb2d5eadd62cc40160d80b52e10b0c103bfbcc
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-21 17:08:22 +0100

    update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir ~/var/www/efs
 #cloud-config
 package_upgrade: true
 packages:
@@ -8,7 +7,6 @@ httpd
 php
 runcmd:
 echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-657731ae.efs.eu-west-1.amazonaws.com:/    /var/www/html/efs   nfs4    defaults" >> /etc/fstab
-mkdir /var/www/html/efs
 mount -a
 service httpd start
 chkconfig httpd on
