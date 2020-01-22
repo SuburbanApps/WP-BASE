0fc00bb10d7aea4c4b909bef171e15e2a8e7b24a
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-22 11:58:34 +0100

    tst user data and delete 1/3 subnets

--- userdata.sh
+++ userdata.sh
@@ -3,5 +3,3 @@ yum-config-manager --enable epel
 yum update -y
 yum -y install nfs-utils
 mkdir /mnt/efs
-mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${aws_efs_file_system.efs-wp-base.id}.efs.${eu-west-1}.amazonaws.com:/ /mnt/efs
-echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${aws_efs_file_system.efs-wp-base.id}.efs.${eu.west-1}.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
