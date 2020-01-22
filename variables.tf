1c0f83203ab9f9e6d390e7a62552fb44bf697af9
Alejandro Daza Iglesias <adaza@vectoritcgroup.com>
2020-01-22 13:24:35 +0100

    tst

--- variables.tf
+++ variables.tf
@@ -5,8 +5,8 @@ locals  {
 
   aws_region = "eu-west-1"
   vpc_id = "vpc-dc76d7bb"
-  private_subnet_ids = ["subnet-650ae83e", "subnet-ea19bc8d",""] #"subnet-c9cd6f80"
-  public_subnet_ids = ["subnet-c009eb9b", "subnet-971cb9f0",""]#"subnet-48d67401"
+  private_subnet_ids = ["subnet-650ae83e","subnet-ea19bc8d"] #"subnet-c9cd6f80"
+  public_subnet_ids = ["subnet-c009eb9b","subnet-971cb9f0"]#"subnet-48d67401"
   key_pair = "dv10-wp-base"
 
 
