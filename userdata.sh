1dc1ec3e7e9a26c9d53a5cf008d8a26a889512ed
Alejandro Daza Iglesias <adaza@vectorim.net>
2020-01-22 10:27:59 +0000

    Updated userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
-mkdir ~/.aws
-touch ~/.aws/config
-cd ~/var/www/efs
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar zxf latest.tar.gz
-cd wordpress
-sudo cp -rpf * ../
-cd ../
-sudo rm -rf wordpress/ latest.tar.gz
-    
+yum-config-manager --enable epel
+yum update -y
+yum -y install nfs-utils
+mkdir /mnt/efs
+mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${aws_efs_file_system.efs-wp-base.id.id}.efs."${eu.west-1}".amazonaws.com:/ /mnt/efs
+echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${aws_efs_file_system.efs-wp-base.id}.efs."${eu.west-1}".amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
